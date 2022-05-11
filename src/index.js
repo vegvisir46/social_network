557fe0a60eaf5a79e0107e03f731a1354c4105a7
vegvisir <alexandr.rostow@gmail.com>
2022-05-11 21:17:21 +0700

    refactor: state to store

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from "./redux/state";
+import store from "./redux/store";
 import {BrowserRouter} from "react-router-dom";
 
 

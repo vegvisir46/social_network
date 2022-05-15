1329da829dda5bb15e96917054fc169eafacba51
vegvisir <alexandr.rostow@gmail.com>
2022-05-15 10:55:48 +0700

    to class component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,49 @@ import React from "react";
 import styles from './MyPosts.module.css'
 import Post from "./Post/Post";
 
+// class MyPosts extends React.Component {
+//
+//
+//     addPost = () => {
+//         this.props.addPost();
+//     };
+//
+//     onPostChange = (newPostElement) => {
+//         let text = newPostElement.current.value;
+//         this.props.updateNewPostText(text);
+//     };
+//
+//     render() {
+//         let postElements = this.props.profilePage.postsData
+//             .map(post => <Post message={post.message} likesCount={post.likesCount}/>);
+//         let newPostElement = React.createRef();
+//
+//         return (
+//             <div>
+//                 <div className={styles.posts}>
+//                     <div className={styles.postsTitle}>Posts</div>
+//                     <input
+//                         ref={newPostElement}
+//                         onChange={this.onPostChange}
+//                         className={styles.postsInput}
+//                         type="text"
+//                         value={this.props.profilePage.newPostText}/>
+//                     {/*<textarea className={styles.postsInput}*/}
+//                     {/*          onChange={onPostChange}*/}
+//                     {/*          ref={newPostElement}*/}
+//                     {/*          value={props.newPostText}/>*/}
+//                     <div className={styles.postsBtn}>
+//                         <button onClick={this.addPost}>Add Post</button>
+//                     </div>
+//                 </div>
+//                 {postElements}
+//             </div>
+//         );
+//     }
+//
+// }
+
+
 const MyPosts = (props) => {
 
     let postElements = props.profilePage.postsData

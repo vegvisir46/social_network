ba75c6e72735541941842e7372d853dc8671b250
vegvisir <alexandr.rostow@gmail.com>
2022-05-22 11:29:06 +0700

    switch r-r-d to 5.2.0

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import ProfileContainer from './components/Profile/ProfileContainer';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+// import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
@@ -20,17 +21,15 @@ const App = () => {
             <Header/>
             <Navbar/>
             <div className='app-wrapper-content'>
-                <Routes>
-                    <Route path="/profile/*"
-                           element={<ProfileContainer/>}/>
-                    <Route path="/dialogs/*"
-                           element={<DialogsContainer/>}/>
-                    <Route path="/news/*" element={<News/>}/>
-                    <Route path="/music/*" element={<Music/>}/>
-                    <Route path="/users/*"
-                           element={<UsersContainer/>}/>
-                    <Route path="/settings/*" element={<Settings/>}/>
-                </Routes>
+                    <Route path="/profile/:userId?"
+                           render={ () => <ProfileContainer /> }/>
+                    <Route path="/dialogs/"
+                           render={ () => <DialogsContainer /> }/>
+                    <Route path="/news/" render={ () => <News/>}/>
+                    <Route path="/music/" render={ () => <Music/>}/>
+                    <Route path='/users'
+                           render={ () => <UsersContainer /> }/>
+                    <Route path="/settings/" render={ () => <Settings/>}/>
                 {/*<Profile/>*/}
                 {/*<Dialogs/>*/}
             </div>
@@ -40,3 +39,38 @@ const App = () => {
 };
 
 export default App;
+
+
+
+
+
+
+
+// ========= for v6 r-r-d =========
+// const App = () => {
+//
+//     return (
+//         <div className='app-wrapper'>
+//             <Header/>
+//             <Navbar/>
+//             <div className='app-wrapper-content'>
+//                 <Routes>
+//                     <Route path="/profile/:userId?"
+//                            render={ () => <ProfileContainer /> }/>
+//                     <Route path="/dialogs/*"
+//                            element={<DialogsContainer/>}/>
+//                     <Route path="/news/*" element={<News/>}/>
+//                     <Route path="/music/*" element={<Music/>}/>
+//                     <Route path="/users/*"
+//                            element={<UsersContainer/>}/>
+//                     <Route path="/settings/*" element={<Settings/>}/>
+//                 </Routes>
+//                 {/*<Profile/>*/}
+//                 {/*<Dialogs/>*/}
+//             </div>
+//
+//         </div>
+//     );
+// };
+//
+// export default App;
